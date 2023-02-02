cd41c8f0828c746998fdd2f32b05cffe4c25ced2
Miguel Moreno <amiguelmoreno42@gmail.com>
2023-02-02 19:47:02 +0100

    testing with get next line in main file

--- includes/fdf.h
+++ includes/fdf.h
@@ -1,18 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Fdf.h                                              :+:      :+:    :+:   */
+/*   fdf.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: antmoren <antmoren@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 20:09:53 by antmoren          #+#    #+#             */
-/*   Updated: 2023/02/01 20:25:32 by antmoren         ###   ########.fr       */
+/*   Updated: 2023/02/02 14:37:55 by antmoren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
 
-# include "MLX42/MLX42.h"
+# define WIDTH 512
+# define HEIGHT 512
+
+# include "../lib/MLX42/include/MLX42/MLX42.h"
+# include <fcntl.h>
+# include <memory.h>
+# include <stdio.h>
+# include <stdlib.h>
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,18 +6,13 @@
 /*   By: antmoren <antmoren@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 20:08:35 by antmoren          #+#    #+#             */
-/*   Updated: 2023/02/01 20:31:18 by antmoren         ###   ########.fr       */
+/*   Updated: 2023/02/02 11:38:04 by antmoren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "MLX42/MLX42.h"
-#include <memory.h>
-#include <stdio.h>
-#include <stdlib.h>
-#define WIDTH 512
-#define HEIGHT 512
+#include "../includes/fdf.h"
 
-static mlx_image_t	*img;
+/* static mlx_image_t	*img;
 
 void	hook(void *param)
 {
@@ -26,30 +21,64 @@ void	hook(void *param)
 	mlx = param;
 	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
 		mlx_close_window(mlx);
-	if (mlx_is_key_down(mlx, MLX_KEY_UP))
-		img->instances[0].y -= 5;
-	if (mlx_is_key_down(mlx, MLX_KEY_DOWN))
-		img->instances[0].y += 5;
-	if (mlx_is_key_down(mlx, MLX_KEY_LEFT))
-		img->instances[0].x -= 5;
-	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
-		img->instances[0].x += 5;
 }
 
 int32_t	main(void)
 {
-	mlx_t *mlx;
+	mlx_t	*mlx;
 
 	if (!(mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true)))
 		return (EXIT_FAILURE);
-
 	img = mlx_new_image(mlx, 128, 128);
 	memset(img->pixels, 255, img->width * img->height * sizeof(int));
 	mlx_image_to_window(mlx, img, 0, 0);
-
+	mlx_image_to_window(mlx, img, 50, 50);
 	mlx_loop_hook(mlx, &hook, mlx);
 	mlx_loop(mlx);
-
 	mlx_terminate(mlx);
 	return (EXIT_SUCCESS);
 }
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Driver code
+#include <stdio.h>
+
+int	main(int argc, char **argv)
+{
+	int		fd;
+	char	*str;
+
+	fd = open(argv[1], O_RDONLY);
+	str = get_next_line(fd);
+	ft_printf("%d\n", fd);
+	ft_printf("%s\n", str);
+	return (0);
+}
+
+/* int	main(int argc, char **argv)
+{
+	FILE	*demo;
+	int		display;
+
+	// pointer demo to FILE
+	// Creates a file "demo_file"
+	// with file access as read mode
+	demo = fopen(argv[1], "r");
+	// loop to extract every characters
+	while (1)
+	{
+		// reading file
+		display = fgetc(demo);
+		// end of file indicator
+		if (feof(demo))
+			break ;
+		// displaying every characters
+		printf("%c", display);
+	}
+	// closes the file pointed by demo
+	fclose(demo);
+	return (0);
+} */
